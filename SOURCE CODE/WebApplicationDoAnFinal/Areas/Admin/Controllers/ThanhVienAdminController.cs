6031dcd68679bdb2e219c78f1d620c1faae482b5
phthao2003 <thaodhy145@gmail.com>
2023-05-30 18:07:37 +0700

    Upload project

---
+++ SOURCE CODE/WebApplicationDoAnFinal/Areas/Admin/Controllers/ThanhVienAdminController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplicationDoAnFinal.Models.BUS;
+
+namespace WebApplicationDoAnFinal.Areas.Admin.Controllers
+{
+    public class ThanhVienAdminController : Controller
+    {
+        // GET: Admin/NhaPhanPhoi
+        public ActionResult Index()
+        {
+            return View(ShopOnlineBUS.DanhSachTV());
+        }
+
+        // GET: Admin/NhaPhanPhoi/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Admin/NhaPhanPhoi/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/NhaPhanPhoi/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Admin/NhaPhanPhoi/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Admin/NhaPhanPhoi/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Admin/NhaPhanPhoi/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Admin/NhaPhanPhoi/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
